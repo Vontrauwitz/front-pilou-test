b1c32d6b825d79edec3b82b5e4dfb55a0b3c376a
Hans Trauwitz <hans.trauwitz@gmail.com>
2023-07-27 15:24:44 -0600

    ajustes genrales

--- src/pages/users.js
+++ src/pages/users.js
@@ -1,5 +1,7 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 import React, { useEffect, useState } from 'react';
 import { getAllUsers } from './api/users.api';
+import Image from 'next/image';
 
 export default function Users() {
   const [users, setUsers] = useState([]);
@@ -19,8 +21,15 @@ export default function Users() {
 
   return (
     <>
+
       {users.map((user) => (
+
         <div className="border border-solid border-black rounded-lg p-4" key={user.id}>
+          <img
+            alt="profile photo"
+            src={user.image}
+            className="w-[5%] rounded-full  drop-shadow-[2px_3px_2px_rgba(255,255,255,.4)]"
+          />
           <dl className="divide-y divide-gray-100 text-sm">
             <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
               <dt className="font-medium text-gray-900">Nombre</dt>
